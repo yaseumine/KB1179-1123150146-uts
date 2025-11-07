8454396d547c7571007cec4404d631e1d5744d82
yaseumine <yasminmaharani123@gmail.com>
2025-11-07 09:36:58 +0700

    membuat box circle di splashscreen 1 serta membuat memperbaiki tata letak images di pusbec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uts/splashscreen1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
         useMaterial3: true,
       ),
+      home: const Splashscreen1(),
     );
   }
 }
--- lib/splashscreen1.dart
+++ lib/splashscreen1.dart
@@ -5,14 +5,25 @@ class Splashscreen1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final cs = Theme.of(context).colorScheme;
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text("Welcome to the App"),
-            SizedBox(height: 20),
-            CircularProgressIndicator(),
+          children: [
+            const SizedBox(height: 50),
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: cs.surface,
+                image: const DecorationImage(
+                  image: AssetImage('images/assets/image1.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
           ],
         ),
       ),
