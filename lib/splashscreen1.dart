f0439f6c90d02a267ac1ee38163e777ada67d43f
yaseumine <yasminmaharani123@gmail.com>
2025-11-07 09:00:49 +0700

    nembuat splashscreen 1

---
+++ lib/splashscreen1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Splashscreen1 extends StatelessWidget {
+  const Splashscreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
