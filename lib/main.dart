d77327561e2ad112deafddb1b6e9ca0dde061633
yaseumine <yasminmaharani123@gmail.com>
2025-11-07 13:30:47 +0700

    menambahkan debugshowbanner false di main dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '1123150146',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
